bae8d8a676e5b1727e7f81e6e26ae313f6c1eba6
amaskaikin <anton.maskaikin@gmail.com>
2017-04-13 11:03:45 +0300

    Update ProcessResvMsg.py

--- src/rsvp/ProcessResvMsg.py
+++ src/rsvp/ProcessResvMsg.py
@@ -36,35 +36,42 @@ def generate_resv_tear(data, req_id):
 def process_resv(data):
     Logger.logger.info('Processing Resv Message. . .')
     # TODO: move logic to htb, get request parameters from there by id
-    id = get_layer(data, Const.CL_MSG_ID).getfieldval('Data')
+    key = get_layer(data, Const.CL_MSG_ID).getfieldval('Data')
+    req = ReservationRequest.Instance()
     # req = get request parameters from htb by id
     # req.src_ip, req.dst_ip, req.tos, req.speed = ...
-    req = ReservationRequest.Instance()
-    req.src_ip = '192.168.0.109'
-    req.dst_ip = '192.168.0.106'
-    req.tos = 8
-    req.speed = 100
-    ip = data.getlayer(0).getfieldval('dst')
-    is_sender = ip == req.src_ip
-    if is_sender:
-        Logger.logger.info('Resv message reached the sender.')
-        Logger.logger.info('Full Reservation completed successfully')
-        return
-    if not any((req.src_ip, req.dst_ip, req.tos, req.speed)):
-        Logger.logger.info('Error! Path is broken')
-        error_msg = str(get_current_hop(req.src_ip)) + ': Error! Path is broken'
+    is_class, ret = get_class(key)
+    if is_class:     
+        req.src_ip = ret[0]
+        req.dst_ip = ret[1]
+        req.tos = ret[2]
+        req.speed = ret[3]
+        Logger.logger.info('[Resv] Reservation request: src ip: ' + str(req.src_ip) + ', dst ip: ' + str(req.dst_ip) +
+                           ', tos: ' + str(req.tos) + ', rate: ' + str(req.speed))
+        ip = data.getlayer(0).getfieldval('dst')
+        is_sender = ip == req.src_ip
+        if is_sender:
+            Logger.logger.info('Resv message reached the sender.')
+            Logger.logger.info('Full Reservation completed successfully')
+            return
+        if not any((req.src_ip, req.dst_ip, req.tos, req.speed)):
+            Logger.logger.info('Error! Path is broken')
+            error_msg = str(get_current_hop(req.src_ip)) + ': Error! Path is broken'
+            send_error(req.src_ip, data, error_msg, 'resv')
+            return
+    else:
+        Logger.logger.info('Htb class not found')
+        error_msg = str(get_current_hop(req.src_ip)) + ret
         send_error(req.src_ip, data, error_msg, 'resv')
         return
-    Logger.logger.info('[Resv] Reservation request: src ip: ' + str(req.src_ip) + ', dst ip: ' + str(req.dst_ip) +
-                       ', tos: ' + str(req.tos) + ', rate: ' + str(req.speed))
-    is_reserved = reserve(req)
+    is_reserved, ret = reserve(req)
     Logger.logger.info('[Resv] : req_src ip: ' + str(req.src_ip) + ', src ip: ' + str(get_current_hop(req.src_ip)))
     if is_reserved:
         Logger.logger.info('Reservation success')
         send_next_hop(req.src_ip, data, 'Resv')
     else:
         Logger.logger.info('Reservation failed')
-        error_msg = str(get_current_hop(req.src_ip)) + ': Reservation failed'
+        error_msg = str(get_current_hop(req.src_ip)) + ret
         send_error(req.src_ip, data, error_msg, 'resv')
 
 
@@ -93,5 +100,3 @@ def process_resv_tear(data):
         send_next_hop(req.src_ip, data, 'ResvTear')
     else:
         Logger.logger.info('Destroying failed')
-
-
